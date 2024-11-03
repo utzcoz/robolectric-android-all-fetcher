a8763d988b0e3eee934e7494ad870624946237cc
utzcoz <utzcoz@outlook.com>
2024-11-03 19:26:19 +0800

    Apply Spotless
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- gradle/android-all-jars-metadata-extractor/extractor/build.gradle.kts
+++ gradle/android-all-jars-metadata-extractor/extractor/build.gradle.kts
@@ -3,16 +3,21 @@ import java.security.MessageDigest
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     `java-library`
+    id("com.diffplug.spotless") version "7.0.0.BETA4"
+}
+
+spotless {
+    kotlinGradle {
+        target("*.gradle.kts") // default target for kotlinGradle
+        ktlint()
+    }
 }
 
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
     google()
-}
-
-dependencies {
-    implementation(libs.robolectric)
+    gradlePluginPortal()
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -27,22 +32,23 @@ java {
 // be synced here.
 // This version is from 4.14-beta-1
 private val preInstrumentedVersion = 7
-private val androidAllJars = listOf(
-    Pair("5.0.2_r3", "r0"),
-    Pair("5.1.1_r9", "r2"),
-    Pair("6.0.1_r3", "r1"),
-    Pair("7.0.0_r1", "r1"),
-    Pair("8.0.0_r4", "r1"),
-    Pair("8.1.0", "4611349"),
-    Pair("9", "4913185-2"),
-    Pair("10", "5803371"),
-    Pair("11", "6757853"),
-    Pair("12", "7732740"),
-    Pair("12.1", "8229987"),
-    Pair("13", "9030017"),
-    Pair("14", "10818077"),
-    Pair("15", "12543294")
-)
+private val androidAllJars =
+    listOf(
+        Pair("5.0.2_r3", "r0"),
+        Pair("5.1.1_r9", "r2"),
+        Pair("6.0.1_r3", "r1"),
+        Pair("7.0.0_r1", "r1"),
+        Pair("8.0.0_r4", "r1"),
+        Pair("8.1.0", "4611349"),
+        Pair("9", "4913185-2"),
+        Pair("10", "5803371"),
+        Pair("11", "6757853"),
+        Pair("12", "7732740"),
+        Pair("12.1", "8229987"),
+        Pair("13", "9030017"),
+        Pair("14", "10818077"),
+        Pair("15", "12543294"),
+    )
 
 /**
  * Calculates sha256 string for a maven dependency.
@@ -50,7 +56,7 @@ private val androidAllJars = listOf(
  * It uses stream to update digest calculation one by one to avoid large memory usage when
  * dependency file is very large.
  */
-fun calculateFileSha256(artifactFile: File) : String {
+fun calculateFileSha256(artifactFile: File): String {
     val digest = MessageDigest.getInstance("SHA-256")
     artifactFile.inputStream().use { inputStream ->
         val buffer = ByteArray(1024 * 1024)
@@ -60,7 +66,7 @@ fun calculateFileSha256(artifactFile: File) : String {
         }
     }
     val hashBytes = digest.digest()
-    val hashString = hashBytes.joinToString("") { String.format("%02x", it.toInt() and 0xFF ) }
+    val hashString = hashBytes.joinToString("") { String.format("%02x", it.toInt() and 0xFF) }
     println("SHA-256 of ${artifactFile.name}: $hashString")
     return hashString
 }
@@ -75,12 +81,12 @@ tasks.register<Copy>("updateAndroidAllJarsMetadata") {
             println("Configure android-all $version")
             androidAllConfiguration(
                 "org.robolectric:android-all:" +
-                    "${version.first}-robolectric-${version.second}"
+                    "${version.first}-robolectric-${version.second}",
             )
             println("Configure android-all-instrumented $version")
             preinstrumentedAndroidAllConfiguration(
                 "org.robolectric:android-all-instrumented:" +
-                        "${version.first}-robolectric-${version.second}-i$preInstrumentedVersion"
+                    "${version.first}-robolectric-${version.second}-i$preInstrumentedVersion",
             )
         }
 
